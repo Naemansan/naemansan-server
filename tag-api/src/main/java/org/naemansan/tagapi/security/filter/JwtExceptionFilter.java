81d5afd21a3fdd717e1dc8ab7b78003d85b09855
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-12-12 03:26:50 +0900

    feat: tag intercepter 완료

---
+++ tag-api/src/main/java/org/naemansan/tagapi/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,62 @@
+package org.naemansan.tagapi.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+            filterChain.doFilter(request, response);
+
+//        try {
+//            filterChain.doFilter(request, response);
+//        } catch (SecurityException e) {
+//            log.error("FilterException throw SecurityException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.ACCESS_DENIED);
+//            filterChain.doFilter(request, response);
+//        } catch (MalformedJwtException e) {
+//            log.error("FilterException throw MalformedJwtException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.TOKEN_MALFORMED_ERROR);
+//            filterChain.doFilter(request, response);
+//        } catch (IllegalArgumentException e) {
+//            log.error("FilterException throw IllegalArgumentException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.TOKEN_TYPE_ERROR);
+//            filterChain.doFilter(request, response);
+//        } catch (ExpiredJwtException e) {
+//            log.error("FilterException throw ExpiredJwtException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.EXPIRED_TOKEN_ERROR);
+//            filterChain.doFilter(request, response);
+//        } catch (UnsupportedJwtException e) {
+//            log.error("FilterException throw UnsupportedJwtException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.TOKEN_UNSUPPORTED_ERROR);
+//            filterChain.doFilter(request, response);
+//        } catch (JwtException e) {
+//            log.error("FilterException throw JwtException Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.TOKEN_UNKNOWN_ERROR);
+//            filterChain.doFilter(request, response);
+//        } catch (CommonException e) {
+//            log.error("FilterException throw Exception Exception : {}", e.getMessage());
+//            request.setAttribute("exception", e.getErrorCode());
+//            filterChain.doFilter(request, response);
+//        } catch (Exception e) {
+//            log.error("FilterException throw Exception Exception : {}", e.getMessage());
+//            request.setAttribute("exception", ErrorCode.INTERNAL_SERVER_ERROR);
+//            filterChain.doFilter(request, response);
+//        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return true;
+    }
+}
