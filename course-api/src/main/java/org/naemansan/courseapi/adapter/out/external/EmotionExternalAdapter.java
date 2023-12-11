c4610839f81b24cf4203e3d6e318959145d58d18
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-12-12 04:07:57 +0900

    feat: auth-api init

--- course-api/src/main/java/org/naemansan/courseapi/adapter/out/external/EmotionExternalAdapter.java
+++ course-api/src/main/java/org/naemansan/courseapi/adapter/out/external/EmotionExternalAdapter.java
@@ -4,12 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.naemansan.common.annotaion.WebAdapter;
 import org.naemansan.courseapi.application.port.out.EmotionServicePort;
 import org.naemansan.courseapi.dto.type.EEmotion;
+import org.naemansan.courseapi.utility.ExternalClientUtil;
 import org.naemansan.courseapi.utility.InternalClientUtil;
 
 @WebAdapter
 @RequiredArgsConstructor
 public class EmotionExternalAdapter implements EmotionServicePort {
-    private final InternalClientUtil internalClientUtil;
+    private final ExternalClientUtil externalClientUtil;
 
     @Override
     public EEmotion analyzeContent(String content) {
